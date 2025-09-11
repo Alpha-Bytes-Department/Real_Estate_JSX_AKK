fe3aa2b67b4e9b69e2adb2a10a968274c0bf6c26
Saikh Seraj Fahim <saikhseraj2017@gmail.com>
2025-09-11 06:56:34 +0600

    viewAllImage

---
+++ src/app/view_all_image/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function ViewAllImage() {
+    return (
+        <div className="flex flex-col items-center justify-center gap-6 p-8">
+            <Image src="/HomePageLogo.svg" alt="home-page-logo" height={100} width={200} />
+            <Image src="/home_pic_1.jpg" alt="home_pic_1" height={200} width={400} />
+            <Image src="/home_pic_2.jpg" alt="home_pic_2" height={200} width={400} />
+            <button className="bg-[#000000] text-[#FFFFFF] font-poppins px-6 py-2 rounded-md
+            cursor-pointer">Back</button>
+        </div>
+    );
+}
