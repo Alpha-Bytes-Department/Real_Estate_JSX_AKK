4a67589c5651163f6fe97a818015f2c59804a194
Saikh Seraj Fahim <saikhseraj2017@gmail.com>
2025-09-18 07:56:08 +0600

    dashboard-account-shadcn

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 import { useState } from "react";
 import { Slider } from "@/components/ui/slider"
 import Link from "next/link";
-
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
 
 export default function Dashboard() {
     const [zoningMapClicked, setZoningMapClicked] = useState(false);
@@ -192,52 +193,69 @@ export default function Dashboard() {
 
             {
                 account && (
-                    <div className="absolute top-25 right-20 w-[250px] h-[350px] flex flex-col gap-4
-                    bg-[#FFFFFF]">
-                        <Link href="/account-settings">
-                            <button className="w-full h-[40px] flex items-center justify-between 
-                                bg-[#D9D9D9] mt-3 rounded-sm cursor-pointer">
-                                <p className="text-[#000000] font-poppins pl-3">Profile Settings</p>
-                                <Image src="user-logo.svg" alt="user-logo" width={20} height={20}
-                                    className="mr-3" />
-                            </button>
-                        </Link>
-
-                        <Link href="/saved-properties">
-                            <button className="w-full h-[40px] flex items-center justify-between 
-                            bg-[#D9D9D9] rounded-sm cursor-pointer">
-                                <p className="text-[#000000] font-poppins pl-3">Saved Properties</p>
-                                <Image src="bookmark.svg" alt="bookmark-logo" width={20} height={20}
-                                    className="mr-3" />
-                            </button>
-                        </Link>
-
-                        <Link href="/listings">
-                            <button className="w-full h-[40px] flex items-center justify-between 
-                            bg-[#D9D9D9] rounded-sm cursor-pointer">
-                                <p className="text-[#000000] font-poppins pl-3">Listings</p>
-                                <Image src="list.svg" alt="list-logo" width={20} height={20}
-                                    className="mr-3" />
-                            </button>
-                        </Link>
-
-                        <div className="w-full h-[40px] flex items-center gap-2 pl-4">
-                            <div className="w-[80px] h-[1px] bg-[#8F8C8C]" />
-                            <p className="text-[#A4A3A3] font-poppins">and</p>
-                            <div className="w-[80px] h-[1px] bg-[#8F8C8C]" />
+                    <div className="absolute top-25 right-20 w-full max-w-[250px] h-[350px] 
+                    flex flex-col gap-4 bg-[#FFFFFF]">
+                        <div className="w-full pt-6">
+                            <Link href="/account-settings">
+                                <Button className="w-full flex justify-between cursor-pointer bg-[#D9D9D9] 
+                            hover:bg-[#D9D9D9]">
+                                    <Label htmlFor="profile-settings" className="text-[#000000] font-poppins 
+                                cursor-pointer text-base">Profile Settings</Label>
+                                    <Image src="/user-logo.svg" alt="user-logo" height={15} width={20}
+                                        className="" />
+                                </Button>
+                            </Link>
                         </div>
 
-                        <button>
-                            <div className="w-full h-[40px] bg-[#D9D9D9] rounded-sm cursor-pointer">
-                                <p className="text-[#000000] font-poppins pt-2 pl-3">Privacy Policy</p>
-                            </div>
-                        </button>
+                        <div className="w-full">
+                            <Link href="/saved-properties">
+                                <Button className="w-full flex justify-between cursor-pointer bg-[#D9D9D9] 
+                            hover:bg-[#D9D9D9]">
+                                    <Label htmlFor="profile-settings" className="text-[#000000] 
+                                    font-poppins cursor-pointer text-base">Saved Properties</Label>
+                                    <Image src="/bookmark.svg" alt="bookmark-logo" height={15} width={20}
+                                        className="" />
+                                </Button>
+                            </Link>
+                        </div>
 
-                        <button>
-                            <div className="w-full h-[40px] bg-[#D9D9D9] rounded-sm cursor-pointer">
-                                <p className="text-[#000000] font-poppins pt-2 pl-3">Terms of Services</p>
-                            </div>
-                        </button>
+                        <div className="w-full">
+                            <Link href="/listings">
+                                <Button className="w-full flex justify-between cursor-pointer bg-[#D9D9D9] 
+                            hover:bg-[#D9D9D9]">
+                                    <Label htmlFor="listings" className="text-[#000000] font-poppins 
+                                cursor-pointer text-base">Listings</Label>
+                                    <Image src="/list.svg" alt="list-logo" height={15} width={20}
+                                        className="" />
+                                </Button>
+                            </Link>
+                        </div>
+
+                        <div className="w-full h-[40px] flex items-center gap-2 pl-4">
+                            <div className="w-full max-w-[80px] h-[1px] bg-[#8F8C8C]" />
+                            <p className="text-[#A4A3A3] font-poppins">and</p>
+                            <div className="w-full max-w-[80px] h-[1px] bg-[#8F8C8C]" />
+                        </div>
+
+                        <div className="w-full">
+                            <Link href="">
+                                <Button className="w-full flex items-center justify-center 
+                                    cursor-pointer bg-[#D9D9D9] hover:bg-[#D9D9D9]">
+                                    <Label htmlFor="privacy-policy" className="text-[#000000] 
+                                        font-poppins cursor-pointer text-base">Privacy Policy</Label>
+                                </Button>
+                            </Link>
+                        </div>
+
+                        <div className="w-full">
+                            <Link href="">
+                                <Button className="w-full flex items-center justify-center 
+                                    cursor-pointer bg-[#D9D9D9] hover:bg-[#D9D9D9]">
+                                    <Label htmlFor="terms-of-services" className="text-[#000000] 
+                                        font-poppins cursor-pointer text-base">Terms of Services</Label>
+                                </Button>
+                            </Link>
+                        </div>
                     </div>
                 )
             }
